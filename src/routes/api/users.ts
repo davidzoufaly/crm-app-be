ab83324ea71860234a97e24254a1768a0d48bee0
David Zoufalý <davidzoufaly@gmail.com>
2019-10-24 17:11:26 +0200

    registration completed

--- src/routes/api/users.ts
+++ src/routes/api/users.ts
@@ -20,96 +20,166 @@ const msges = {
   exist: "Exist"
 };
 
-
 //? Register
 routerUsers.post("/", (req, res) => {
-    client.connect((err, client) => {
-      if (err) throw err;
-      console.log(err);
-      const dbTarget = client.db(db).collection(collection);
-      try {
-        dbTarget.findOneAndUpdate({username: req.body.username}, {$setOnInsert: req.body}, {upsert: true}, (_, data) => {
-            data.value === null
-                ? res.status(200).json({msg: msges.success})
-                : res.status(200).json({msg: msges.exist})
-        })
-      } catch (err) {
-        console.log(err);
-        res.status(400).json({ msg: msges.error });
-      }
-    });
-  });
+  client.connect((err, client) => {
+    if (err) throw err;
+    console.log(err);
+    const dbTarget = client.db(db).collection(collection);
+    try {
+        //check if user exists, if not insert him, else do nothing and return msg
+      dbTarget.findOneAndUpdate(
+        { username: req.body.username },
+        { $setOnInsert: req.body },
+        { upsert: true },
+        (_, data) => {
+          // no data came back -> user does not exist and was created
+          if (data.value === null) {
+            // continue to setup new user own DB
+            const dbTarget = client.db(req.body.username);
 
-//create DB
-routerUsers.post("/new-db2", (req, res) => {
-    client.connect((err, client) => {
-        if (err) throw err;
-        console.log(err);
-        const dbTarget = client.db(req.body.id);
-
-        const defaultFields = [
-            {
+            const defaultFields = [
+              {
                 fieldName: "First name",
                 fieldType: "text",
                 fieldPermanent: true,
                 fieldFormVisible: null,
                 fieldOptions: [],
                 fieldInForm: false
-            },
-            {
+              },
+              {
                 fieldName: "Last name",
                 fieldType: "text",
                 fieldPermanent: true,
                 fieldFormVisible: null,
                 fieldOptions: [],
                 fieldInForm: false
-            },
-            {
+              },
+              {
                 fieldName: "Email",
                 fieldType: "text",
                 fieldPermanent: true,
                 fieldFormVisible: null,
                 fieldOptions: [],
                 fieldInForm: false
-            },
-            {
+              },
+              {
                 fieldName: "Date added",
                 fieldType: "text",
                 fieldPermanent: true,
                 fieldFormVisible: null,
                 fieldOptions: [],
                 fieldInForm: false
-            },
-            {
+              },
+              {
                 fieldName: "Last modified",
                 fieldType: "text",
                 fieldPermanent: true,
                 fieldFormVisible: null,
                 fieldOptions: [],
                 fieldInForm: false
-            }
-        ]
+              }
+            ];
 
-        Promise
-        .all([
-            dbTarget.createCollection("clients"),
-            dbTarget.createCollection("emails"),
-            dbTarget.collection("settings").insertOne({
+            Promise.all([
+              dbTarget.createCollection("clients"),
+              dbTarget.createCollection("emails"),
+              dbTarget.collection("settings").insertOne({
                 settingsName: "emailSettings",
                 username: "",
                 pass: ""
-            }),
-            dbTarget.collection("fields").insertMany(defaultFields)
-        ])
-        .then(() => {
-            return res.send({ success: true });
-        })
-        .catch((reason) => {
-            console.log(reason);
-            return res.status(400).send({ reason: 'unknown' });
-        });
-    });
+              }),
+              dbTarget.collection("fields").insertMany(defaultFields)
+            ])
+              .then(() => {
+                res.status(200).json({ msg: msges.success });
+              })
+              .catch(reason => {
+                console.log(reason);
+                res.status(400).send({ msg: msges.error });
+              });
+          } else {
+            res.status(200).json({ msg: msges.exist });
+          }
+        }
+      );
+    } catch (err) {
+      console.log(err);
+      res.status(400).json({ msg: msges.error });
+    }
+  });
 });
+
+//create DB
+// routerUsers.post("/new-db2", (req, res) => {
+//     client.connect((err, client) => {
+//         if (err) throw err;
+//         console.log(err);
+//         const dbTarget = client.db(req.body.id);
+
+//         const defaultFields = [
+//             {
+//                 fieldName: "First name",
+//                 fieldType: "text",
+//                 fieldPermanent: true,
+//                 fieldFormVisible: null,
+//                 fieldOptions: [],
+//                 fieldInForm: false
+//             },
+//             {
+//                 fieldName: "Last name",
+//                 fieldType: "text",
+//                 fieldPermanent: true,
+//                 fieldFormVisible: null,
+//                 fieldOptions: [],
+//                 fieldInForm: false
+//             },
+//             {
+//                 fieldName: "Email",
+//                 fieldType: "text",
+//                 fieldPermanent: true,
+//                 fieldFormVisible: null,
+//                 fieldOptions: [],
+//                 fieldInForm: false
+//             },
+//             {
+//                 fieldName: "Date added",
+//                 fieldType: "text",
+//                 fieldPermanent: true,
+//                 fieldFormVisible: null,
+//                 fieldOptions: [],
+//                 fieldInForm: false
+//             },
+//             {
+//                 fieldName: "Last modified",
+//                 fieldType: "text",
+//                 fieldPermanent: true,
+//                 fieldFormVisible: null,
+//                 fieldOptions: [],
+//                 fieldInForm: false
+//             }
+//         ]
+
+//         Promise
+//         .all([
+//             dbTarget.createCollection("clients"),
+//             dbTarget.createCollection("emails"),
+//             dbTarget.collection("settings").insertOne({
+//                 settingsName: "emailSettings",
+//                 username: "",
+//                 pass: ""
+//             }),
+//             dbTarget.collection("fields").insertMany(defaultFields)
+//         ])
+//         .then(() => {
+//             return res.send({ success: true });
+//         })
+//         .catch((reason) => {
+//             console.log(reason);
+//             return res.status(400).send({ reason: 'unknown' });
+//         });
+//     });
+// });
 
 // login
 routerUsers.post("/authenticate-user", (req, res) => {
@@ -119,11 +189,11 @@ routerUsers.post("/authenticate-user", (req, res) => {
     const dbTarget = client.db(db).collection(collection);
     try {
       dbTarget.findOne({ username: req.body.username }, (err, data) => {
-          data === null
-            ? res.status(200).json({ msg: msges.error })
-            : data.password === req.body.password
-                ? res.status(200).json({ msg: msges.success })
-                : res.status(200).json({ msg: msges.error })
+        data === null
+          ? res.status(200).json({ msg: msges.error })
+          : data.password === req.body.password
+          ? res.status(200).json({ msg: msges.success })
+          : res.status(200).json({ msg: msges.error });
         if (err) throw err;
       });
       client.close();
