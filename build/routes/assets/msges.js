e7c4c84ca596cc454d596eec80025a3a761fc427
David Zoufalý <davidzoufaly@gmail.com>
2019-10-28 22:46:16 +0100

    ffffff

--- build/routes/assets/msges.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const msges = {
-    success: "Success",
-    error: "Error",
-    credentialsNotSet: "Setup your email settings first",
-    exist: "Exist"
-};
-exports.default = msges;
--- build/routes/middlewares/Cors.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Cors = (req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "DELETE, POST, GET, OPTIONS, PUT");
-    next();
-};
-exports.default = Cors;
