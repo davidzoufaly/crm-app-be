0d27a2fc2d844b78e4821ca0d6470cf5e1c83b3a
David Zoufalý <davidzoufaly@gmail.com>
2019-10-28 22:03:52 +0100

    f

--- build/Logger.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Logger = function (req, res, next) {
-    console.log(req.protocol + "://" + req.get('host') + req.originalUrl);
-    next();
-};
-module.exports = Logger;
