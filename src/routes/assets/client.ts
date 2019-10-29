34e9c840f80e7b36d2e3474073729eaa0dcc5f50
David Zoufalý <davidzoufaly@gmail.com>
2019-10-29 11:16:34 +0100

    mongo atlas

--- src/routes/assets/client.ts
+++ src/routes/assets/client.ts
@@ -3,9 +3,9 @@ const MongoClient = mongodb.MongoClient;
 require('dotenv').config()
 
 //? ATLAS CONNECTION
-// const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PW}@cluster0-g9fj3.mongodb.net/admin?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PW}@cluster0-g9fj3.mongodb.net/admin?retryWrites=true&w=majority`;
 
-const client = new MongoClient(process.env.LOCALHOST, {
+const client = new MongoClient(uri, {
   useUnifiedTopology: true,
   useNewUrlParser: true
 });
