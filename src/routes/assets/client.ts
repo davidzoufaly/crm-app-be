7ec13f8ae4aade7e29bf4d9b4a29c77cb97d9a09
David Zoufalý <davidzoufaly@gmail.com>
2019-10-29 14:23:59 +0100

    ff

--- src/routes/assets/client.ts
+++ src/routes/assets/client.ts
@@ -3,7 +3,7 @@ const MongoClient = mongodb.MongoClient;
 require('dotenv').config()
 
 //? ATLAS CONNECTION
-const uri = `mongodb+srv://${process.env.ATLAS_USER}:${process.env.ATLAS_PW}@cluster0-g9fj3.mongodb.net/admin?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://RW:mongodb://localhost:27017/admin@cluster0-g9fj3.mongodb.net/admin?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, {
   useUnifiedTopology: true,
